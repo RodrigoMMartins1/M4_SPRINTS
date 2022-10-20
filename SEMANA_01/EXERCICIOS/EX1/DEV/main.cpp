0c03e93981c53888f16066827ee69c8f8da6ccf5
RodrigoMMartins1 <99209230+RodrigoMMartins1@users.noreply.github.com>
2022-10-20 15:44:41 -0300

    EXERCÍCOS SEMANA 1
    EXERCÍCIOS SEMANA 1 ADICIONADOS

--- SEMANA_01/EXERCICIOS/EX1/DEV/main.cpp
+++ SEMANA_01/EXERCICIOS/EX1/DEV/main.cpp
@@ -1,87 +1,106 @@
-#include <iostream>
-#include <string>
-
-// 1 -  Faça uma função que recebe uma certa medida e ajusta ela percentualmente 
+// 1 -  Faça uma função que recebe uma certa medida e ajusta ela percentualmente
 // entre dois valores mínimo e máximo e retorna esse valor
 
-// 2 - Faça uma função que simule a leitura de um sensor lendo o 
-// valor do teclado ao final a função retorna este valor
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
-// 3 - Faça uma função que armazena uma medida inteira qualquer 
-// em um vetor fornecido. Note que como C não possui vetores 
-// nativos da linguagem, lembre-se que você precisa passar o 
-// valor máximo do vetor assim como a última posição preenchida
-// Evite também que, por acidente, um valor seja escrito em 
-// uma área de memória fora do vetor
+//EXERCÍCIO 1'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''{
+float percentage(float esc, float max, float min) {
+  float result = 0;
+  float sub1 = esc - min;
+  float sub2 = max - min;
 
-
-
-// 4 - Faça uma função que recebe um vetor com 4 posições que contém 
-// o valor da distância de um pequeno robô até cada um dos seus 4 lados.
-// A função deve retornar duas informações: A primeira é a direção 
-// de maior distância ("Direita", "Esquerda", "Frente", "Tras") e a 
-// segunda é esta maior distância.
-
-
-
-
-// 5 - Faça uma função que pergunta ao usuário se ele deseja continuar o mapeamento e 
-// retorna verdadeiro ou falso
-
-
-// 6 - A função abaixo (que está incompleta) vai "dirigindo" virtualmente um robô 
-// e através de 4 sensores em cada um dos 4 pontos do robo ("Direita", "Esquerda", 
-// "Frente", "Tras"). 
-//      A cada passo, ele verifica as distâncias aos objetos e vai mapeando o terreno 
-// para uma movimentação futura. 
-//      Ele vai armazenando estas distancias em um vetor fornecido como parâmetro 
-// e retorna a ultima posicao preenchida do vetor.
-//      Esta função deve ir lendo os 4 sensores até que um comando de pare seja enviado 
-//
-//      Para simular os sensores e os comandos de pare, use as funções já construídas 
-// nos ítens anteriores e em um looping contínuo até que um pedido de parada seja 
-// enviado pelo usuário. 
-//
-//      Complete a função com a chamada das funções já criadas
-int dirige(int *v,int maxv){
-	int maxVetor = maxv;
-	int *vetorMov = v;
-	int posAtualVetor = 0;
-	int dirigindo = 1;		
-	while(dirigindo){		
-		int medida = /// .. Chame a função de de leitura da medida para a "Direita"
-		medida = converteSensor(medida,0,830);
-		posAtualVetor = // Chame a função para armazenar a medida no vetor
-        ///////////////////////////////////////////////////////////////////////////		
-		// Repita as chamadas acima para a "Esquerda", "Frente", "Tras"
-		// ................
-		///////////////////////////////////////////////////////////////////////////
-		dirigindo = leComando();		
-	}
-	return posAtualVetor;
+  result = 100 * (sub1 / sub2);
+  return result;
 }
 
 
-// O trecho abaixo irá utilizar as funções acima para ler os sensores e o movimento
-// do robô e no final percorrer o vetor e mostrar o movimento a cada direção baseado 
-// na maior distância a cada movimento
-void percorre(int *v,int tamPercorrido){		
-	int *vetorMov = v;
-	int maiorDir = 0;
-	
-	for(int i = 0; i< tamPercorrido; i+=4){
-		char *direcao = direcaoMenorCaminho(&(vetorMov[i]),&maiorDir);
-		printf("Movimentando para %s distancia = %i\n",direcao,maiorDir);
-	}
+//2 - Faça uma função que recebe um número inteiro e retorna este valor inserido em formato de return
+int value(int num) {
+  return num;
 }
 
-int main(int argc, char** argv) {
-	int maxVetor = 100;
-	int vetorMov[maxVetor*4];
-	int posAtualVet = 0;
-	
-	posAtualVet = dirige(vetorMov,maxVetor);
-	percorre(vetorMov,posAtualVet);
-	
-	return 0;
+//3 - Faça uma função que que insere novos valores em um vetor cujos argumentos serão: valor a ser inserido, capacidade de armazenamento de valores (número de posições), qual foi a última posição preenchida e o nome do vetor criado. A função funcionará como um "append" do Python controlado
+int append(int num, int cap, int last, int *vet) {
+  if (last < cap) {
+    vet[last] = num;
+    last++;
+  }
+  return last;
+}
+//4 - Faça uma função que recebe um vetor com 4 valores numéricos que representam "distâncias": Direita, esquerda, frente e trás e retorna qual destes valores (direita, etc) é o maior.
+//i.e: 3,5,1,6: saída: trás
+//Faça outra função que retorne este maior valor do vetor
+//i.e.: 3,5,1,6: saída: 6
+int maior(int *vet) {
+  int maior = vet[0];
+  for (int i = 0; i < 4; i++) {
+    if (vet[i] > maior) {
+      maior = vet[i];
+    }
+  }
+  return maior;
+}
+
+//5 - Faça uma função que lê os valores 0 e 1, ficando em loop até que o valor 0 seja lido
+int loop() {
+  int num = 1;
+  while (num != 0) {
+    printf("Digite um número: ");
+    scanf("%d", &num);
+  }
+  return 0;
+}
+
+//6 - Interpretação subjetiva. Robô lê um vetor semelhante ao item 4 que define a ordem de execução de movimentos de um robô. Armazene a posição e a distância para qual o robô andou em um vetor (ou dicionário, ou struct, etc).
+string movimentos(int *vet) {
+  string mov = "";
+  for (int i = 0; i < 4; i++) {
+    if (vet[i] == 0) {
+      mov += "Frente ";
+    } else if (vet[i] == 1) {
+      mov += "Trás ";
+    } else if (vet[i] == 2) {
+      mov += "Direita ";
+    } else if (vet[i] == 3) {
+      mov += "Esquerda ";
+    }
+  }
+  return mov;
+}
+//i.e.: 4,3,21
+//Direita
+//Esquerda
+//Frente
+//Trás
+//Armazenamento: {Direita:4; Esquerda:3, Frente:2, Trás:1}
+
+
+// TESTE DE VALIDAÇÃO DOS EXERCÍCIOS
+int main(){
+// EXERCÍCIO 1:
+
+    float teste = percentage(80, 100, 60);
+    cout << teste << endl;
+//EXERCÍCIO 2:
+        int teste2 = value(10);
+        cout << teste2 << endl;
+        
+//EXERCÍCIO 3:
+        int test3 = append(10, 10, 0, &teste2);
+        cout << test3 << endl;
+
+//EXERCÍCIO 4:
+        int vet[4] = {3, 5, 1, 6};
+        int teste4 = maior(vet);
+        cout << teste4 << endl;
+//EXERCÍCIO 5:
+        int teste5 = loop();
+        cout << teste5 << endl;
+//
+
+return 0;
 }
